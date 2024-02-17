93f48d442813c74320d8a992e2be1f75af3d4ee2
Vasyl Yakymets <Vasia911119@gmail.com>
2024-02-17 22:47:33 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,10 @@ const onLamp = document.querySelector(".img-on");
 const switchCord = document.querySelector(".switch-cord");
 
 const switchLamp = () => {
-  // Перевіряємо, чи лампа увімкнена
   if (onLamp.classList.contains("visually-hidden")) {
-    // Якщо лампа вимкнена, вмикаємо її
     onLamp.classList.remove("visually-hidden");
     offLamp.classList.add("visually-hidden");
   } else {
-    // Якщо лампа увімкнена, вимикаємо її
     onLamp.classList.add("visually-hidden");
     offLamp.classList.remove("visually-hidden");
   }
